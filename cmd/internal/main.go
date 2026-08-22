37b6bcd99fde8436e46584e1557e939c9aa23093
Isvane <277444536+Isvane@users.noreply.github.com>
2026-08-22 23:26:49 +0700

    feat(kafka): add Kafka producer creation helper

--- cmd/internal/main.go
+++ cmd/internal/main.go
@@ -37,6 +37,48 @@ func createTopic(broker string, topicName string, partitions int32, replicationF
 	return nil
 }
 
+func createProducer(broker string) (sarama.AsyncProducer, error) {
+	config := sarama.NewConfig()
+
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+
+	producer, err := sarama.NewAsyncProducer([]string{broker}, config)
+	if err != nil {
+		log.Fatalf("Error creating producer: %v", err)
+	}
+
+	go func() {
+		for msg := range producer.Successes() {
+			fmt.Printf("[SUCCESS] Message sent to topic %s [partition: %d, offset: %d]\n", msg.Topic, msg.Partition, msg.Offset)
+		}
+	}()
+
+	go func() {
+		for err := range producer.Errors() {
+			log.Printf("[ERROR] Failed to send message: %v\n", err)
+		}
+	}()
+
+	messages := []string{"Staryu evolved into Starmie!"}
+
+	go func() {
+		for _, pokemon := range messages {
+			msg := &sarama.ProducerMessage{
+				Topic: "Pokemon",
+				Value: sarama.StringEncoder(pokemon),
+			}
+
+			producer.Input() <- msg
+			fmt.Printf("Pushed %s to producer channel\n", pokemon)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	return producer, nil
+}
+
 func main() {
 	broker := os.Getenv("BROKER")
 	if broker == "" {
@@ -49,6 +91,12 @@ func main() {
 	if err := createTopic(broker, "Pokemon", 1, 1); err != nil {
 		log.Printf("Error creating topic: %v\n", err)
 	}
+
+	producer, err := createProducer(broker)
+	if err != nil {
+		log.Fatalf("Producer error: %v", err)
+	}
+	defer producer.AsyncClose()
 
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
